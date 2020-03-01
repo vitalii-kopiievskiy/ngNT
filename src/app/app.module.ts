626572a0edd542d2704aa720efeac741aeef6309
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2020-03-01 13:25:09 +0200

    add banner, slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
